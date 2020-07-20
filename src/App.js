4689f1d8a38ee71afdcd18d2a31277f423f6ad28
Abrahamcepe <abrahamgolf@gmail.com>
2020-07-20 08:37:10 -0500

    Added Greeter.js component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import NavBar from './NavBar';
-import logo from './logo.svg';
+import Greeter from './Greeter';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <NavBar />
       <h1>Hello World - React!</h1>
-      <NavBar/>
+      <Greeter name="Karen" age="19" excitement={4}/>
+      <Greeter name="Abraham" age="20" excitement={2}/>
     </div>
   );
 }
