f7a6e7ab1eb1938bd73e615463e8b934f9432cf8
Abrahamcepe <abrahamgolf@gmail.com>
2020-07-19 20:11:06 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from './NavBar';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World - React!</h1>
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
